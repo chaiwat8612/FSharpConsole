ac538b933cf82049c818c2ad981cc3dac55c7831
chaiwat8612 <89202968+chaiwat8612@users.noreply.github.com>
2022-07-19 22:31:08 +0700

    For to Loop

--- FSharpConsole/Program.fs
+++ FSharpConsole/Program.fs
@@ -26,13 +26,23 @@ let main argv =
     //printfn $"'num1' is {result}"
 
     //if_else
-    let num1 =40
+    //let num1 =40
 
-    if num1 = 20 then
-        printfn $"num1 equal 40"
-    elif num1 = 50 then
-        printfn $"num1 equal 50"
-    else
-        printfn $"num1 equal 40"
+    //if num1 = 20 then
+    //    printfn $"num1 equal 40"
+    //elif num1 = 50 then
+    //    printfn $"num1 equal 50"
+    //else
+    //    printfn $"num1 equal 40"
+
+    //for to loop
+    //for i = 1 to 10 do
+    //   printf "%d " i
+    //printfn ""
+
+    let mutable num1 = 10
+    for i = 1 to 10 do
+        printf "%d " num1
+        num1 <- num1 + 10        
 
     0 // return an integer exit code
