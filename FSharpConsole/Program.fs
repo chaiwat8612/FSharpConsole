b0b6aa81608378c6f47323b41d9143e09c14ff15
chaiwat8612 <89202968+chaiwat8612@users.noreply.github.com>
2022-07-19 21:54:52 +0700

    variable and show value

--- FSharpConsole/Program.fs
+++ FSharpConsole/Program.fs
@@ -4,5 +4,10 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+
+    //printfn "Hello World from F#!"
+
+    let num1 = 40
+    printfn $"'num1' is {num1}"
+
     0 // return an integer exit code
