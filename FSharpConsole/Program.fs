b84f7e549d2e1b0bd01eb3a9f8ae394887d39be8
chaiwat8612 <89202968+chaiwat8612@users.noreply.github.com>
2022-07-19 21:57:33 +0700

    change value

--- FSharpConsole/Program.fs
+++ FSharpConsole/Program.fs
@@ -7,7 +7,12 @@ let main argv =
 
     //printfn "Hello World from F#!"
 
-    let num1 = 40
+    //let num1 = 40
+    //printfn $"'num1' is {num1}"
+
+    let mutable num1 = 40
+    num1 <- 50
+
     printfn $"'num1' is {num1}"
 
     0 // return an integer exit code
