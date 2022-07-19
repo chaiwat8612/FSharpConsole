a0313e5846ee14f536ff040b4aeeaa2d37b14414
chaiwat8612 <89202968+chaiwat8612@users.noreply.github.com>
2022-07-19 22:01:05 +0700

    operation

--- FSharpConsole/Program.fs
+++ FSharpConsole/Program.fs
@@ -7,12 +7,22 @@ let main argv =
 
     //printfn "Hello World from F#!"
 
+    //variable and show value
     //let num1 = 40
     //printfn $"'num1' is {num1}"
 
-    let mutable num1 = 40
-    num1 <- 50
+    //change value
+    //let mutable num1 = 40
+    //num1 <- 50
 
-    printfn $"'num1' is {num1}"
+    //num1 <- 50
+
+    //printfn $"'num1' is {num1}"
+
+    //operator
+    let num1 = 40
+    let num2 = 50
+    let result = num1 + num2
+    printfn $"'num1' is {result}"
 
     0 // return an integer exit code
